99ded6836f291ddc2f3e6cef1ee037e79d48a7a2
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-12-03 10:37:53 +0500

    Add files via upload

---
+++ class01/app.js
@@ -0,0 +1,54 @@
+// alert("2" - 2);
+//Data types
+//String ""
+//Number 12124
+//Boolean True/false
+// var name = "Minahil";
+// var fatherName = "Irfan";
+// alert(name + " " + FatherName);
+var studentName = "Saylani's student";
+console.log(studentName)
+
+
+///Familiar operators
+// var num = 5;
+// var num2 = "3";
+// alert(num * num2);
+
+
+// var num1 = 2;
+// var num2 = "2";
+// alert(4 + num1 + num2 );
+
+///Unfamiliar operators
+// var num = 5;
+// alert(num);
+// num = num - 1;
+// alert(num);
+// num = num - 1;
+// alert(num);
+// num = num - 1;
+// alert(num);
+
+
+///POST Increment/Decrement
+// var num = 5;
+// alert(num);
+// num--;
+// alert(num)
+// num--;
+// alert(num);
+
+
+///Pre Increment/Decrement
+// var num = 1;
+// alert(--num);
+
+var a = 4;
+var b = 5;
+// alert(a++ + ++a - b-- * a++);
+// //     2  +   4  -    3  *   4
+
+// alert((--b * ++b) + a++ - --a + ++a - b++);
+//    (4   *    5) + 4 -   4  + 5   - 5
+
