1af10780570c81b84c641c3feee684ed27d849c5
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-12-05 10:22:34 +0500

    Add files via upload

---
+++ class02/app.js
@@ -0,0 +1,46 @@
+//prompt
+// var email = prompt("Enter yoyr email", "xyz@.com");
+// var password = prompt("Enter your password");
+// document.write(email);
+// document.write(password);
+
+// var num = +prompt("Enter num 1");
+// var num2 = +prompt("Enter num 2");
+// console.log(num + num2);
+
+
+//IF Statements
+// var city = prompt("ENTER YOUR CITY");
+// if (city === "karachi") {
+//     alert("Welcome to the karachi")
+// }
+
+
+///Comparison operators
+// if (1 === "1") {
+//     alert("True")
+// }
+// if (1 == "1") {
+//     alert("True")
+// }
+
+//NOT EQUAL
+// if (1 !== "1") {
+//     alert("True")
+// }
+// if (1 != "1") {
+//     alert("True")
+// }
+
+//if...else and else if statements
+// var city = prompt("Enter your city");
+// if (city === "karachi") {
+//     console.log("Your are allow");
+// }else{
+//     console.log("you r not allow");
+// }
+
+//QUESTION#01
+// alert("Welcome here")
+
+//QUESTION#02
