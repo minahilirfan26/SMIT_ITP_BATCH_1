56a2d5f61b012ccabf5822f58665931529c4a5cb
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-12-19 10:10:11 +0500

    Add files via upload

---
+++ class07/app.js
@@ -0,0 +1,47 @@
+// var trafficLight = prompt("enter the traffic light here");
+// if (trafficLight === "red") {
+//     console.log("You should stop");
+// }else if(trafficLight === "yellow"){
+//     console.log("Ready to go");
+// }else if(trafficLight === "green"){
+//     console.log("Now yoyu can go");
+// }else{
+//     console.log("please enter correct traffic lights colors");
+// }
+
+//Testing sets of condition
+// var skills = prompt("Enter your skills here");
+// if (skills === "html" || skills === "css" || skills === "bootstrap") {
+//     console.log("Designer");
+// }
+
+// var frontendSkills = prompt("Enter your frontend skills here");
+// var backendSkills = prompt("Enter ur backend skills here")
+// if (frontendSkills === "react" && backendSkills === "mongodb" ) {
+//     console.log("Full stack developer");
+// }
+
+//if else nested
+// var frontendSkills = prompt("Enter your frontend skills here");
+// var backendSkills = prompt("Enter ur backend skills here");
+
+// if (frontendSkills === "react") {
+//     if (backendSkills === "nodejs") {
+//         console.log("Full stack developer");
+//     }
+//     else{
+//         console.log("Not hired")
+//     }
+// }else{
+//     console.log("ur r Not hired")
+// }
+
+//Array
+// var arr = [];
+// var pet = "cat";
+// var pet1 = "dog";
+// var pet2 = "Duck"
+var arr = ["Cat" , "Dog", "Duck", "Parrot"];
+console.log(arr);
+arr[1] = "puppy";
+console.log(arr);
