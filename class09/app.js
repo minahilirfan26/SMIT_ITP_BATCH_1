bfd3d6b644c98a44ae1e3ca40bb15b55b77386cf
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-12-24 09:57:41 +0500

    Add files via upload

---
+++ class09/app.js
@@ -0,0 +1,21 @@
+// var arr = ["string", 25426, true];
+// console.log(arr)
+// var city = ["karachi" , "lahore" , "quetta"];
+// city[0] = "Peshawar";
+// console.log(city[2]);
+// console.log(city.pop());
+// console.log(city.push("Islamabad" , "hyderabda"));
+// console.log(city)
+
+// city.shift();
+// console.log(city.unshift("Peshawar" , "hyderabad"));
+// console.log(city);
+// var city = ["karachi" , "lahore" , "quetta"];
+//  city.splice(0 , 0 , "peshwar" , "ISlamabad" );
+
+//  console.log(city.slice(1));
+
+// for(var i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+// console.log("SMIT");
