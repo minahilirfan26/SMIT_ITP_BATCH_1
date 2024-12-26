3e91a5a631bb76bbefb728d2ea5c4c60013ea85b
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-12-26 10:40:37 +0500

    Add files via upload

---
+++ class10/app.js
@@ -0,0 +1,26 @@
+// var arr = ["Huma", "Erum", "Rubab", "Saliva", "Ayesha"];
+// for(var i = 0; i < arr.length; i++){
+//  console.log(arr[i]);
+// }
+
+// var favNum = +prompt("Enter your fav num");
+// for(var i = 1; i <= 10; i++){
+//     // console.log(i)
+//     console.log(favNum + " x " + i + " = " + favNum*i);
+// }
+
+// for(i = 10; i >= 0; i--){
+//     console.log(i);
+// }
+
+// var word = prompt("Enter a word");
+// var storeWords = "";
+
+// for(i = word.length -1; i >= 0; i--){
+//     storeWords += word[i];
+// }
+// if (storeWords === word) {
+//     console.log("Its palindrum word " + storeWords);
+// }else{
+//     console.log("Its not palindrum " + storeWords)
+// }
