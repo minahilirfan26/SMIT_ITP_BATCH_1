986d8d8495de2be1c6ceb195a06248917d28a279
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-01-02 10:37:57 +0500

    Add files via upload

---
+++ class11/ap.js
@@ -0,0 +1,55 @@
+// for(var i = 0; i <= 10; i++){
+//   console.log(i)
+// }
+// console.log("Hello");
+
+// var arr = ["Marwa", "Uqba", "Laiba", "Ayesha"];
+// // console.log(arr[3]);
+// for(var i = 0; i < arr.length; i++){
+//     console.log(i ,arr[i]);
+// }
+
+// for(var i = 5; i >= 0; i--){
+//   console.log(i)
+//   break;
+// }
+
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+// for(var i = 0; i < firstNames.length; i++){
+//     for(var j = 0; j < lastNames.length; j++){
+//         console.log(firstNames[i]+ " " + lastNames[j]);
+//     }
+// }
+
+
+// for(i = 1; i <= 5; i++){
+//     for(j = 1; j <= 5; j++){
+//         console.log(i , j)
+//     }
+// }
+
+// var city = prompt("Enter ur city name");
+
+// if (city.toUpperCase() === "KARACHI") {
+//     console.log("Welcome to city of lights" + " " + city);
+// }else{
+//     console.log("try again");
+// }
+
+// const myPromise = new Promise((res,rej)=>{
+//     const success = true;
+//     if(success){
+//         res("Promise resolved")
+//     } else {
+//         rej("Promise rejected")
+//     }
+// })
+// .then(result => {
+//     console.log(result);
+// })
+// .catch(error =>{
+//     console.log(error)
+// })
