0f6c2bf2dcb2aaa217e9f1c625341c28a078c80d
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-01-09 10:26:59 +0500

    Add files via upload

---
+++ class14/app.js
@@ -0,0 +1,34 @@
+// var smit = "Saylani mass it SMIT Saylani Saylani";
+// console.log(smit.charAt(10));
+// console.log(smit.indexOf("mass"));
+// console.log(smit.lastIndexOf("S"));
+
+// console.log(smit.length)
+
+// var word = smit.replaceAll("Saylani", "SMIT");
+// console.log(word)
+
+// var num = 12.53098;
+// var scoreAvg = Math.round(num);
+// var scoreAvg = Math.ciel(num);
+// var scoreAvg = Math.floor(num);
+// var scoreAvg = num.toFixed(3);
+// console.log(scoreAvg)
+
+// var num = "2383468";
+// console.log(typeof num);
+// console.log(+num);
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
+// console.log(Number(num));
+
+// var num = 2383468;
+// var str = num.toString();
+// console.log(typeof str);
+
+var date = new Date();
+date.setFullYear(2023);
+console.log(date)
+// console.log(date.getFullYear());
+// var days = ["sun", "mon", "tues" , "wed" , "thurs" , "fri", "sat"];
+// console.log(days[date.getDay()])
