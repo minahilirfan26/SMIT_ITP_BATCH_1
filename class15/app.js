89a41f5fe6e52b91cd661603ad1420237a2747e3
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-01-11 10:30:29 +0500

    Add files via upload

---
+++ class15/app.js
@@ -0,0 +1,68 @@
+// function xyz() {
+//     console.log("SMIT");
+// }
+// xyz();
+// var msg = prompt("Enter ur name");
+// console.log(msg);
+// xyz()
+
+// function xyz(num,num1) {
+//     console.log(num + num1);
+// }
+// xyz(2,5);
+// var popUp = alert;
+// popUp("hello")
+
+// function xyz(popUp) {
+//     popUp("Hello")
+// }
+// xyz(alert)
+
+// var log = console.log;
+// log("Hello")
+
+// var girlNames = ["huma", "laiba", "ayesha"];
+// console.log(girlNames.indexOf("esha"))
+// var smit = "Saylani";
+// console.log(smit.indexOf("m"))
+
+
+// var arr = [7689,7899,1358]
+// function xyz(studentId) {
+//     if (arr.indexOf(studentId) != -1) {
+//         console.log("Present");
+//     }else{
+//         console.log("absent");
+//     }
+// }
+// var idNum = +prompt("enter a num")
+// xyz(idNum);
+
+
+// var count = 0;//5
+// function xyz(){
+    
+//     for(var i = 0; i < 5; i++){
+//     count++;
+//         console.log(count);
+//     }
+// }
+
+// xyz();
+// xyz();
+
+
+function xyz(){
+    var count = 0;
+    for(var i = 0; i < 5; i++){
+    count++;
+        console.log(count);
+    }
+}
+
+xyz();
+xyz();
+xyz()
+
+
+
