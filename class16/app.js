678f71a279909b522b64d7e87146cd6843f1ec0f
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-01-16 10:32:12 +0500

    Add files via upload

---
+++ class16/app.js
@@ -0,0 +1,49 @@
+// var days = prompt("Enter your fav day");
+
+// switch (days) {
+//     case "sun":
+//         console.log("sunday");
+//         break;
+//     case "sat":
+//             console.log("saturday")  
+//             break
+//             case "fri":
+//                 console.log("friday")  
+//                 break
+//     default:
+//         console.log("try again")
+//         break;
+// }
+
+// switch (days) {
+//     case "sun":
+//     case "sat": 
+//         console.log("Holiday");
+//             break;
+//             case "fri":
+//                 console.log("friday")  
+//                 break
+//     default:
+//         console.log("try again")
+//         break;
+// }
+
+// if (condition) {
+    
+// } else {
+    
+// }
+
+///Revision
+
+var fullName = "minahil";
+console.log(fullName.slice(0,1).toUpperCase() + fullName.slice(1));
+
+
+console.log(box)
+
+
+function submit() {
+    var input = document.getElementById("input").value;
+    var box = document.getElementById("box").innerHTML = "<h1>" + input + "</h1>";
+}
